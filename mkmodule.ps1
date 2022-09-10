840931bf46b6966a96309f23b3fc98a642b75c21
Jens Horstmeier <jens.horstmeier.external@fujitsu.com>
2022-09-10 15:12:06 +0200

    Fixed problem with naming

--- mkmodule.ps1
+++ mkmodule.ps1
@@ -1,7 +1,7 @@
 
 param(
     [Parameter(Mandatory = $false)]
-    [string]$Module = [System.IO.Path]::GetFileNameWithoutExtension($(Get-Location)),
+    [string]$Module = $null,
     [Parameter(Mandatory = $false)]
     [string]$CompanyName = "Company Name",
     [Parameter(Mandatory = $false)]
@@ -10,24 +10,34 @@ param(
     [string]$Destination = (Get-Location)
 )
 
+$dir = New-Item -Path $Destination -ItemType Directory -Force
+
+
+if ([string]::IsNullOrEmpty($Module)) {
+    Write-Host "Destination is $($dir.Name)"
+    $Module = $dir.Name
+}
+
+Write-Host "Module is $Module"
+
 if ([String]::IsNullOrEmpty($Author)) {
     $Author = $CompanyName
 }
 
-if (-not (Test-Path $Destination)) {
-    New-Item -ItemType Directory -Path $Destination | Out-Null
-}
-
 $guid = [guid]::NewGuid()
-
+$today = Get-Date -Format "%d.%M.%y"
 $scriptDirectory = $PSScriptRoot
 Get-Content -Path "$scriptDirectory/template.psd1"
 | ForEach-Object {
-    $_ -replace '\[Module\]', $Module -replace '\[CompanyName\]', $CompanyName -replace '\[Author\]', $Author -replace '\[Guid\]', $guid
+    $_ -replace '\[Module\]', $Module `
+        -replace '\[CompanyName\]', $CompanyName `
+        -replace '\[Author\]', $Author `
+        -replace '\[Guid\]', $guid `
+        -replace '\[Today\]', $today
 }
 | Out-File -FilePath "$Destination/$Module.psd1"
 
 Copy-Item -Path "$scriptDirectory/template.psm1" -Destination "$Destination/$Module.psm1"
 
-New-Item -ItemType Directory -Path "$Destination/Public" | Out-Null
-New-Item -ItemType Directory -Path "$Destination/Private" | Out-Null
+New-Item -ItemType Directory -Path "$Destination/Public" -Force | Out-Null
+New-Item -ItemType Directory -Path "$Destination/Private" -Force | Out-Null
